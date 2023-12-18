fcc6598ab11afffd347ad8475db3c65add818eb5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-18 19:55:27 -0300

    test: ensure Category instantiates with default values

---
+++ tests/Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using DomainEntity = Codeflix.Catalog.Domain.Entity;
+
+namespace Codeflix.Catalog.UnitTests.Domain.Entity.Category;
+public class CategoryTest
+{
+  [Fact(DisplayName = nameof(InstantiateWithDefaultValues))]
+  [Trait("Domain", "Category - Aggregates")]
+  public void InstantiateWithDefaultValues()
+  {
+    var validData = new
+    {
+      Name = "category name",
+      Description = "category description"
+    };
+    DateTime dateTimeBefore = DateTime.Now;
+    DomainEntity.Category category = new(validData.Name, validData.Description);
+    DateTime dateTimeAfter = DateTime.Now;
+
+    Assert.NotNull(category);
+    Assert.Equal(validData.Name, category.Name);
+    Assert.Equal(validData.Description, category.Description);
+    Assert.NotEqual(default, category.Id);
+    Assert.NotEqual(default, category.CreatedAt);
+    Assert.True(category.CreatedAt < dateTimeAfter);
+    Assert.True(category.CreatedAt > dateTimeBefore);
+    Assert.True(category.IsActive);
+  }
+}

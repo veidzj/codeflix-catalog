66d1e9e6854e9aa6b7a20f37d0d3035ad8d78d12
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-18 20:33:20 -0300

    test: ensure Category instantiates when isActive is provided

--- tests/Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
+++ tests/Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -27,4 +27,29 @@ public class CategoryTest
     Assert.True(category.CreatedAt > dateTimeBefore);
     Assert.True(category.IsActive);
   }
+
+  [Theory(DisplayName = nameof(InstantiateWithIsActive))]
+  [Trait("Domain", "Category - Aggregates")]
+  [InlineData(true)]
+  [InlineData(false)]
+  public void InstantiateWithIsActive(bool isActive)
+  {
+    var validData = new
+    {
+      Name = "category name",
+      Description = "category description"
+    };
+    DateTime dateTimeBefore = DateTime.Now;
+    DomainEntity.Category category = new(validData.Name, validData.Description, isActive);
+    DateTime dateTimeAfter = DateTime.Now;
+
+    Assert.NotNull(category);
+    Assert.Equal(validData.Name, category.Name);
+    Assert.Equal(validData.Description, category.Description);
+    Assert.NotEqual(default, category.Id);
+    Assert.NotEqual(default, category.CreatedAt);
+    Assert.True(category.CreatedAt < dateTimeAfter);
+    Assert.True(category.CreatedAt > dateTimeBefore);
+    Assert.Equal(isActive, category.IsActive);
+  }
 }

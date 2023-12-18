51a5e0203f9562c6506c2c193e4427555a5d434b
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-18 20:33:16 -0300

    feat: ensure Category instantiates when isActive is provided

--- src/Codeflix.Catalog.Domain/Entity/Category.cs
+++ src/Codeflix.Catalog.Domain/Entity/Category.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 
 namespace Codeflix.Catalog.Domain.Entity;
-public class Category(string name, string description)
+public class Category(string name, string description, bool isActive = true)
 {
   public Guid Id { get; private set; } = Guid.NewGuid();
   public string Name { get; private set; } = name;
   public string Description { get; private set; } = description;
-  public bool IsActive { get; private set; } = true;
+  public bool IsActive { get; private set; } = isActive;
   public DateTime CreatedAt { get; private set; } = DateTime.Now;
 }

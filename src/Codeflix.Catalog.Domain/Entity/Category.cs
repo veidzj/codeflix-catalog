8e401967a69dfa9774f0e7a945888fbe1714fd82
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-18 19:55:20 -0300

    feat: ensure Category instantiates with default values

---
+++ src/Codeflix.Catalog.Domain/Entity/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Codeflix.Catalog.Domain.Entity;
+public class Category(string name, string description)
+{
+  public Guid Id { get; private set; } = Guid.NewGuid();
+  public string Name { get; private set; } = name;
+  public string Description { get; private set; } = description;
+  public bool IsActive { get; private set; } = true;
+  public DateTime CreatedAt { get; private set; } = DateTime.Now;
+}

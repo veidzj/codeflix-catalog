6c060b9513dfd1004ae624d1616ac70a1bab01fc
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-02 07:11:24 -0300

    test: ensure DomainValidation NotNull does not throws on success

---
+++ tests/Codeflix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,20 @@
+﻿using Bogus;
+using Codeflix.Catalog.Domain.Validation;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Codeflix.Catalog.UnitTests.Domain.Validation;
+public class DomainValidationTest
+{
+  private Faker Faker { get; set; } = new Faker();
+
+  [Fact(DisplayName = nameof(NotNullDoesNotThrowsOnSuccess))]
+  [Trait("Domain", "DomainValidation - Validation")]
+  public void NotNullDoesNotThrowsOnSuccess()
+  {
+    string value = this.Faker.Commerce.ProductName();
+    Action action = () => DomainValidation.NotNull(value, "Value");
+    action.Should().NotThrow();
+  }
+}

09d107e2c91692122c2a3fb7d5eef8451e111481
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-03 09:53:50 -0300

    feat: ensure DomainValidation.MaxLength  throws when value is greater

--- src/Codeflix.Catalog.Domain/Validation/DomainValidation.cs
+++ src/Codeflix.Catalog.Domain/Validation/DomainValidation.cs
@@ -26,4 +26,12 @@ public class DomainValidation
       throw new EntityValidationException($"{fieldName} should not be less than {minLength} characters long" );
     }
   }
+
+  public static void MaxLength(string target, int maxLength, string fieldName)
+  {
+    if (target.Length > maxLength)
+    {
+      throw new EntityValidationException($"{fieldName} should not be greater than {maxLength} characters long");
+    }
+  }
 }

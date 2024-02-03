69dc11974c9a29b28b223d16ed250c8be8ae72b1
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-02 23:03:09 -0300

    feat: ensure DomainValidation.MinLength throws when value is less than minLength

--- src/Codeflix.Catalog.Domain/Validation/DomainValidation.cs
+++ src/Codeflix.Catalog.Domain/Validation/DomainValidation.cs
@@ -18,4 +18,12 @@ public class DomainValidation
       throw new EntityValidationException($"{fieldName} should not be null or empty");
     }
   }
+
+  public static void MinLength(string target, int minLength, string fieldName)
+  {
+    if (target.Length < minLength)
+    {
+      throw new EntityValidationException($"{fieldName} should not be less than {minLength} characters long" );
+    }
+  }
 }

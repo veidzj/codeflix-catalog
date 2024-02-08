54054d70146bf0af6426f67b26fad11951f7066e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-08 06:50:12 -0300

    test: implement CreateCategory usecase

---
+++ tests/Codeflix.Catalog.UnitTests/Application/CreateCategory/CreateCategoryTest.cs
@@ -0,0 +1,34 @@
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Repository;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading;
+using Xunit;
+using UseCases = Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+
+namespace Codeflix.Catalog.UnitTests.Application.CreateCategory;
+public class CreateCategoryTest
+{
+  [Fact(DisplayName = nameof(CreateCategory))]
+  [Trait("Application", "CreateCategory - Use Cases")]
+  public async void CreateCategory()
+  {
+    Mock<ICategoryRepository> repositoryMock = new();
+    Mock<IUnitOfWork> unitOfWorkMock = new();
+    UseCases.CreateCategory useCase = new(repositoryMock.Object, unitOfWorkMock.Object);
+    UseCases.CreateCategoryInput input = new("Category Name", "Category Description", true);
+
+    UseCases.CreateCategoryOutput output = await useCase.Handle(input, CancellationToken.None);
+
+    repositoryMock.Verify(repository => repository.Insert(It.IsAny<Category>(), It.IsAny<CancellationToken>()), Times.Once);
+    unitOfWorkMock.Verify(unityOfWork => unityOfWork.Commit(It.IsAny<CancellationToken>()), Times.Once);
+    output.Should().NotBeNull();
+    output.Name.Should().Be("Category Name");
+    output.Description.Should().Be("Category Description");
+    output.IsActive.Should().Be(true);
+    output.Id.Should().NotBeEmpty();
+    output.CreatedAt.Should().NotBeSameDateAs(default);
+  }
+}

b31be03e88fcb87c4d60fc25fd8883b822805923
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-08 04:55:40 -0300

    feat: add IUnitOfWork

---
+++ src/Codeflix.Catalog.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Codeflix.Catalog.Application.Interfaces;
+public interface IUnitOfWork
+{
+  public Task Commit(CancellationToken cancellationToken);
+}

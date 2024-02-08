fa51ef77de8f876447da3cc24e020527707fada3
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-08 06:50:04 -0300

    feat: implement CreateCategory usecase

---
+++ src/Codeflix.Catalog.Application/UseCases/Category/CreateCategory/CreateCategory.cs
@@ -0,0 +1,26 @@
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Domain.Repository;
+using System.Threading;
+using System.Threading.Tasks;
+using DomainEntity = Codeflix.Catalog.Domain.Entity;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+public class CreateCategory : ICreateCategory
+{
+  private readonly ICategoryRepository categoryRepository;
+  private readonly IUnitOfWork unitOfWork;
+
+  public CreateCategory(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+  {
+    this.categoryRepository = categoryRepository;
+    this.unitOfWork = unitOfWork;
+  }
+
+  public async Task<CreateCategoryOutput> Handle(CreateCategoryInput input, CancellationToken cancellationToken)
+  {
+    DomainEntity.Category category = new(input.Name, input.Description, input.IsActive);
+    await this.categoryRepository.Insert(category, cancellationToken);
+    await this.unitOfWork.Commit(cancellationToken);
+    return new CreateCategoryOutput(category.Id, category.Name, category.Description, category.IsActive, category.CreatedAt);
+  }
+}
---
+++ src/Codeflix.Catalog.Application/UseCases/Category/CreateCategory/ICreateCategory.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.CreateCategory;
+public interface ICreateCategory
+{
+  public Task<CreateCategoryOutput> Handle(CreateCategoryInput input, CancellationToken cancellationToken);
+}

14fbd2cbac6dd2702d7713bd4a98c972f9732b54
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-18 18:56:26 -0300

    feat: add GetCategoryOutput

---
+++ src/Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryOutput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DomainEntity = Codeflix.Catalog.Domain.Entity;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+public class GetCategoryOutput
+{
+  public Guid Id { get; set; }
+  public string Name { get; set; }
+  public string Description { get; set; }
+  public bool IsActive { get; set; }
+  public DateTime CreatedAt { get; set; }
+
+  public GetCategoryOutput(Guid id, string name, string description, bool isActive, DateTime createdAt)
+  {
+    this.Id = id;
+    this.Name = name;
+    this.Description = description ?? "";
+    this.IsActive = isActive;
+    this.CreatedAt = createdAt;
+  }
+
+  public static GetCategoryOutput FromCategory(DomainEntity.Category category)
+  {
+    return new(category.Id, category.Name, category.Description, category.IsActive, category.CreatedAt);
+  }
+}

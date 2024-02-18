1ed979b53e4af076a69452efeaa61865a95020e0
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-18 19:51:27 -0300

    test: ensure GetCategory returns a category on success

---
+++ tests/Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTest.cs
@@ -0,0 +1,44 @@
+﻿using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Repository;
+using FluentAssertions;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using UseCase = Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+
+namespace Codeflix.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryTest
+{
+  private readonly GetCategoryTestFixture fixture;
+
+  public GetCategoryTest(GetCategoryTestFixture fixture)
+  {
+    this.fixture = fixture;
+  }
+
+  [Fact(DisplayName = nameof(GetCategory))]
+  [Trait("Application", "GetCategory - Use Cases")]
+  public async Task GetCategory()
+  {
+    Mock<ICategoryRepository> repositoryMock = this.fixture.GetRepositoryMock();
+    Category category = this.fixture.GetValidCategory();
+    repositoryMock.Setup(x => x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>())).ReturnsAsync(category);
+    UseCase.GetCategoryInput input = new(category.Id);
+    UseCase.GetCategory useCase = new(repositoryMock.Object);
+
+    UseCase.GetCategoryOutput output = await useCase.Handle(input, CancellationToken.None);
+
+    repositoryMock.Verify(x => x.Get(It.IsAny<Guid>(), It.IsAny<CancellationToken>()), Times.Once);
+
+    output.Should().NotBeNull();
+    output.Name.Should().Be(category.Name);
+    output.Description.Should().Be(category.Description);
+    output.IsActive.Should().Be(category.IsActive);
+    output.Id.Should().Be(category.Id);
+    output.CreatedAt.Should().Be(category.CreatedAt);
+  }
+}
---
+++ tests/Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,51 @@
+﻿using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.UnitTests.Common;
+using Moq;
+using Xunit;
+
+namespace Codeflix.Catalog.UnitTests.Application.GetCategory;
+public class GetCategoryTestFixture : BaseFixture
+{
+  public Mock<ICategoryRepository> GetRepositoryMock()
+  {
+    return new();
+  }
+
+  public Category GetValidCategory()
+  {
+    return new(this.GetValidCategoryName(), this.GetValidCategoryDescription());
+  }
+
+  public string GetValidCategoryName()
+  {
+    string categoryName = "";
+
+    while (categoryName.Length < 3)
+    {
+      categoryName = this.Faker.Commerce.Categories(1)[0];
+    }
+
+    if (categoryName.Length > 255)
+    {
+      categoryName = categoryName[..255];
+    }
+
+    return categoryName;
+  }
+
+  public string GetValidCategoryDescription()
+  {
+    string categoryDescription = this.Faker.Commerce.ProductDescription();
+
+    if (categoryDescription.Length > 10_000)
+    {
+      categoryDescription = categoryDescription[..10_000];
+    }
+
+    return categoryDescription;
+  }
+}
+
+[CollectionDefinition(nameof(GetCategoryTestFixtureCollection))]
+public class GetCategoryTestFixtureCollection : ICollectionFixture<GetCategoryTestFixture> { }

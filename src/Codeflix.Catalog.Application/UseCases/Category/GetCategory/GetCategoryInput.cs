14bdf2121b0e148a6696e1d460bc1b3ece550f0a
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-02-18 19:02:33 -0300

    feat: add GetCategoryInput

---
+++ src/Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInput.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+public class GetCategoryInput : IRequest<GetCategoryOutput>
+{
+  public Guid Id { get; set; }
+
+  public GetCategoryInput(Guid id)
+  {
+    this.Id = id;
+  }
+}

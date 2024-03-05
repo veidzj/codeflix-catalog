41336f538b8aa2f9e5feacd36c9c95a99e705987
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-05 18:52:39 -0300

    feat: ensure GetCategoryInputValidation validates correct input

---
+++ src/Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInputValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+public class GetCategoryInputValidator : AbstractValidator<GetCategoryInput>
+{
+  public GetCategoryInputValidator()
+  {
+    this.RuleFor(x => x.Id).NotEmpty();
+  }
+}

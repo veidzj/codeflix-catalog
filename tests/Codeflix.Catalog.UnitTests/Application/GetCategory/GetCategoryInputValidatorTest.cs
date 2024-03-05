8cb1aae0b5049fb0c093c1f8e4064d47cafa1758
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-05 18:52:46 -0300

    test: ensure GetCategoryInputValidation validates correct input

---
+++ tests/Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
@@ -0,0 +1,32 @@
+﻿using Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+using FluentAssertions;
+using FluentValidation.Results;
+using System;
+using Xunit;
+
+namespace Codeflix.Catalog.UnitTests.Application.GetCategory;
+
+[Collection(nameof(GetCategoryTestFixture))]
+public class GetCategoryInputValidatorTest
+{
+  private readonly GetCategoryTestFixture fixture;
+
+  public GetCategoryInputValidatorTest(GetCategoryTestFixture fixture)
+  {
+    this.fixture = fixture;
+  }
+
+  [Fact(DisplayName = nameof(ValidationOk))]
+  [Trait("Application", "GetCategoryInputValidation - UseCases")]
+  public void ValidationOk()
+  {
+    GetCategoryInput validInput = new(Guid.NewGuid());
+    GetCategoryInputValidator validator = new();
+
+    ValidationResult validationResult = validator.Validate(validInput);
+
+    validationResult.Should().NotBeNull();
+    validationResult.IsValid.Should().BeTrue();
+    validationResult.Errors.Should().HaveCount(0);
+  }
+}

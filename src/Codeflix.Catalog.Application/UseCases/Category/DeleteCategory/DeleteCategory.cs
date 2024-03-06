8f78d40de62c29246ed41c2ab58eb56fd3f4d22f
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-06 10:17:54 -0300

    feat: ensure DeleteCategory does not throws on success

---
+++ src/Codeflix.Catalog.Application/UseCases/Category/DeleteCategory/DeleteCategory.cs
@@ -0,0 +1,27 @@
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Domain.Repository;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using DomainEntity = Codeflix.Catalog.Domain.Entity;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
+public class DeleteCategory : IDeleteCategory
+{
+  private readonly ICategoryRepository categoryRepository;
+  private readonly IUnitOfWork unitOfWork;
+
+  public DeleteCategory(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+  {
+    this.categoryRepository = categoryRepository;
+    this.unitOfWork = unitOfWork;
+  }
+
+  public async Task<Unit> Handle(DeleteCategoryInput request, CancellationToken cancellationToken)
+  {
+    DomainEntity.Category category = await this.categoryRepository.Get(request.Id, cancellationToken);
+    await this.categoryRepository.Delete(category, cancellationToken);
+    await this.unitOfWork.Commit(cancellationToken);
+    return Unit.Value;
+  }
+}
---
+++ src/Codeflix.Catalog.Application/UseCases/Category/DeleteCategory/DeleteCategoryInput.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
+public class DeleteCategoryInput : IRequest
+{
+  public Guid Id { get; set; }
+
+  public DeleteCategoryInput(Guid id)
+  {
+    this.Id = id;
+  }
+}

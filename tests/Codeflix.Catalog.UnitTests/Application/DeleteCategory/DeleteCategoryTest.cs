22cc7347ffa875505e7ad8a531ba8b622a209f73
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-06 10:18:22 -0300

    test: ensure DeleteCategory does not throws on success

---
+++ tests/Codeflix.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTest.cs
@@ -0,0 +1,39 @@
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Repository;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using UseCase = Codeflix.Catalog.Application.UseCases.Category.DeleteCategory;
+
+namespace Codeflix.Catalog.UnitTests.Application.DeleteCategory;
+
+[Collection(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTest
+{
+  private readonly DeleteCategoryTestFixture fixture;
+
+  public DeleteCategoryTest(DeleteCategoryTestFixture fixture)
+  {
+    this.fixture = fixture;
+  }
+
+  [Fact(DisplayName = "")]
+  [Trait("Application", "DeleteCategory - Use Cases")]
+  public async Task DeleteCategory()
+  {
+    Mock<ICategoryRepository> repositoryMock = this.fixture.GetRepositoryMock();
+    Mock<IUnitOfWork> unitOfWorkMock = this.fixture.GetUnitOfWorkMock();
+    Category category = this.fixture.GetValidCategory();
+    repositoryMock.Setup(x => x.Get(category.Id, It.IsAny<CancellationToken>())).ReturnsAsync(category);
+    UseCase.DeleteCategoryInput input = new(category.Id);
+    UseCase.DeleteCategory useCase = new(repositoryMock.Object, unitOfWorkMock.Object);
+
+    await useCase.Handle(input, CancellationToken.None);
+
+    repositoryMock.Verify(x => x.Get(category.Id, It.IsAny<CancellationToken>()), Times.Once);
+    repositoryMock.Verify(x => x.Delete(category, It.IsAny<CancellationToken>()), Times.Once);
+    unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()), Times.Once);
+  }
+}
---
+++ tests/Codeflix.Catalog.UnitTests/Application/DeleteCategory/DeleteCategoryTestFixture.cs
@@ -0,0 +1,57 @@
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.UnitTests.Common;
+using Moq;
+using Xunit;
+
+namespace Codeflix.Catalog.UnitTests.Application.DeleteCategory;
+public class DeleteCategoryTestFixture : BaseFixture
+{
+  public Category GetValidCategory()
+  {
+    return new(this.GetValidCategoryName(), this.GetValidCategoryDescription());
+  }
+
+  public string GetValidCategoryName()
+  {
+    string categoryName = "";
+
+    while (categoryName.Length < 3)
+    {
+      categoryName = this.Faker.Commerce.Categories(1)[0];
+    }
+
+    if (categoryName.Length > 255)
+    {
+      categoryName = categoryName[..255];
+    }
+
+    return categoryName;
+  }
+
+  public string GetValidCategoryDescription()
+  {
+    string categoryDescription = this.Faker.Commerce.ProductDescription();
+
+    if (categoryDescription.Length > 10_000)
+    {
+      categoryDescription = categoryDescription[..10_000];
+    }
+
+    return categoryDescription;
+  }
+
+  public Mock<ICategoryRepository> GetRepositoryMock()
+  {
+    return new();
+  }
+
+  public Mock<IUnitOfWork> GetUnitOfWorkMock()
+  {
+    return new();
+  }
+}
+
+[CollectionDefinition(nameof(DeleteCategoryTestFixture))]
+public class DeleteCategoryTestFixtureCollection : ICollectionFixture<DeleteCategoryTestFixture> { }

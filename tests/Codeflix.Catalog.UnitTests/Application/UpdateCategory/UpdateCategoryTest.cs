64b99ec02bfca7792870106485c6794e04cf693e
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-07 19:41:33 -0300

    test: ensure UpdateCategory updates a category

---
+++ tests/Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTest.cs
@@ -0,0 +1,50 @@
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Application.UseCases.Category.Common;
+using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Repository;
+using FluentAssertions;
+using Moq;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using UseCase = Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+
+namespace Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTest
+{
+  private readonly UpdateCategoryTestFixture fixture;
+
+  public UpdateCategoryTest(UpdateCategoryTestFixture fixture)
+  {
+    this.fixture = fixture;
+  }
+
+  [Fact(DisplayName = nameof(UpdateCategory))]
+  [Trait("Application", "UpdateCategory - Use Cases")]
+  public async Task UpdateCategory()
+  {
+    Mock<ICategoryRepository> repositoryMock = this.fixture.GetRepositoryMock();
+    Mock<IUnitOfWork> unitOfWorkMock = this.fixture.GetUnitOfWorkMock();
+    Category category = this.fixture.GetCategory();
+    repositoryMock.Setup(x => x.Get(category.Id, It.IsAny<CancellationToken>())).ReturnsAsync(category);
+    UseCase.UpdateCategoryInput input = new(
+      category.Id,
+      this.fixture.GetValidCategoryName(),
+      this.fixture.GetValidCategoryDescription(),
+      !category.IsActive
+    );
+    UseCase.UpdateCategory useCase = new(repositoryMock.Object, unitOfWorkMock.Object);
+
+    CategoryModelOutput output = await useCase.Handle(input, CancellationToken.None);
+
+    output.Should().NotBeNull();
+    output.Name.Should().Be(input.Name);
+    output.Description.Should().Be(input.Description);
+    output.IsActive.Should().Be(input.IsActive);
+    repositoryMock.Verify(x => x.Get(category.Id, It.IsAny<CancellationToken>()), Times.Once);
+    repositoryMock.Verify(x => x.Update(category, It.IsAny<CancellationToken>()), Times.Once);
+    unitOfWorkMock.Verify(x => x.Commit(It.IsAny<CancellationToken>()), Times.Once);
+  }
+}
---
+++ tests/Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryTestFixture.cs
@@ -0,0 +1,63 @@
+﻿using Codeflix.Catalog.Application.Interfaces;
+using Codeflix.Catalog.Domain.Entity;
+using Codeflix.Catalog.Domain.Repository;
+using Codeflix.Catalog.UnitTests.Common;
+using Moq;
+using System;
+using Xunit;
+
+namespace Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+public class UpdateCategoryTestFixture : BaseFixture
+{
+  public string GetValidCategoryName()
+  {
+    string categoryName = "";
+
+    while (categoryName.Length < 3)
+    {
+      categoryName = this.Faker.Commerce.Categories(1)[0];
+    }
+
+    if (categoryName.Length > 255)
+    {
+      categoryName = categoryName[..255];
+    }
+
+    return categoryName;
+  }
+
+  public string GetValidCategoryDescription()
+  {
+    string categoryDescription = this.Faker.Commerce.ProductDescription();
+
+    if (categoryDescription.Length > 10_000)
+    {
+      categoryDescription = categoryDescription[..10_000];
+    }
+
+    return categoryDescription;
+  }
+
+  public bool GetRandomBoolean()
+  {
+    return new Random().NextDouble() < 0.5;
+  }
+
+  public Category GetCategory()
+  {
+    return new(this.GetValidCategoryName(), this.GetValidCategoryDescription(), this.GetRandomBoolean());
+  }
+
+  public Mock<ICategoryRepository> GetRepositoryMock()
+  {
+    return new();
+  }
+
+  public Mock<IUnitOfWork> GetUnitOfWorkMock()
+  {
+    return new();
+  }
+}
+
+[CollectionDefinition(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryTestFixtureCollection : ICollectionFixture<UpdateCategoryTestFixture> { }

9c594ec5047f3159e5709ee2db4c48180200c642
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-24 16:53:26 -0300

    feat: ensure UpdateCategory updates only name

--- src/Codeflix.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryInput.cs
+++ src/Codeflix.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryInput.cs
@@ -7,10 +7,10 @@ public class UpdateCategoryInput : IRequest<CategoryModelOutput>
 {
   public Guid Id { get; set; }
   public string Name { get; set; }
-  public string Description { get; set; }
+  public string? Description { get; set; }
   public bool? IsActive { get; set; }
 
-  public UpdateCategoryInput(Guid id, string name, string description, bool? isActive = null)
+  public UpdateCategoryInput(Guid id, string name, string? description = null, bool? isActive = null)
   {
     this.Id = id;
     this.Name = name;

87ce66526e4a47c2556395c14b7d23ca15f51054
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-24 21:16:16 -0300

    feat: ensure UpdateCategory dont validates when guid is empty

---
+++ src/Codeflix.Catalog.Application/UseCases/Category/UpdateCategory/UpdateCategoryInputValidator.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+public class UpdateCategoryInputValidator : AbstractValidator<UpdateCategoryInput>
+{
+  public UpdateCategoryInputValidator()
+  {
+    this.RuleFor(x => x.Id).NotEmpty();
+  }
+}

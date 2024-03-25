f9e06129fcaabc7693cdca928919bad886915dc5
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-24 21:16:26 -0300

    test: ensure UpdateCategory dont validates when guid is empty

---
+++ tests/Codeflix.Catalog.UnitTests/Application/UpdateCategory/UpdateCategoryInputValidatorTest.cs
@@ -0,0 +1,33 @@
+﻿using Codeflix.Catalog.Application.UseCases.Category.UpdateCategory;
+using FluentAssertions;
+using FluentValidation.Results;
+using System;
+using Xunit;
+
+namespace Codeflix.Catalog.UnitTests.Application.UpdateCategory;
+
+[Collection(nameof(UpdateCategoryTestFixture))]
+public class UpdateCategoryInputValidatorTest
+{
+  private readonly UpdateCategoryTestFixture fixture;
+
+  public UpdateCategoryInputValidatorTest(UpdateCategoryTestFixture fixture)
+  {
+    this.fixture = fixture;
+  }
+
+  [Fact(DisplayName = nameof(DontValidateWhenEmptyGuid))]
+  [Trait("Application", "UpdateCategoryInputValidator - Use Cases")]
+  public void DontValidateWhenEmptyGuid()
+  {
+    UpdateCategoryInput input = this.fixture.GetValidInput(Guid.Empty);
+    UpdateCategoryInputValidator validator = new();
+
+    ValidationResult validationResult = validator.Validate(input);
+
+    validationResult.Should().NotBeNull();
+    validationResult.IsValid.Should().BeFalse();
+    validationResult.Errors.Should().HaveCount(1);
+    validationResult.Errors[0].ErrorMessage.Should().Be("'Id' must not be empty.");
+  }
+}

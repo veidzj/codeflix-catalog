facd4abb85a4889196d1d4a905f111b39fa0e8d7
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-03-26 16:11:39 -0300

    feat: ensure ListCategories lists when input does not have all parameters

--- src/Codeflix.Catalog.Application/UseCases/Category/ListCategories/ListCategoriesInput.cs
+++ src/Codeflix.Catalog.Application/UseCases/Category/ListCategories/ListCategoriesInput.cs
@@ -5,5 +5,5 @@ using MediatR;
 namespace Codeflix.Catalog.Application.UseCases.Category.ListCategories;
 public class ListCategoriesInput : PaginatedListInput, IRequest<ListCategoriesOutput>
 {
-  public ListCategoriesInput(int page, int perPage, string search, string sort, SearchOrder dir) : base(page, perPage, search, sort, dir) { }
+  public ListCategoriesInput(int page = 1, int perPage = 15, string search = "", string sort = "", SearchOrder dir = SearchOrder.Asc) : base(page, perPage, search, sort, dir) { }
 }
